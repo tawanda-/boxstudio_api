9f129e5f904119d215ac13d29aa179a3a4bee62a
Tawanda <tmuhwati@gmail.com>
2019-09-22 16:54:45 +0200

    htaccess and index page

--- index.php
+++ index.php
@@ -1,2 +1,19 @@
 <?php
+    ini_set('display_startup_errors', 1);
+    ini_set('display_errors', 1);
+    error_reporting(-1);
+
+    $request = explode('/', trim($_SERVER['REQUEST_URI'], '/'));
+
+    switch ($request[0]) {
+        case '' :
+            require __DIR__.'/indexhtml.php';
+            break;
+        case 'app' :
+            require __DIR__.'/app/router.php';
+            break;
+        default:
+            echo 'not found';
+            break;
+    }
 ?>
---
+++ indexhtml.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Boxing Studio</title>
+    </head>
+    <body style="background: #000;">
+        <div style="text-align: center;display: block;height:100%;width:100%">
+            <div style="width:100%">
+                <img src="https://boxstudio.esikolweni.co.za/resources/images/coding_work_in_progress.gif">
+            </div>
+        </div>
+    </body>
+</html>
